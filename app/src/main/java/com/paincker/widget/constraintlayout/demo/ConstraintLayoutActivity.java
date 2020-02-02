601b4e62e5c86aee5a3c8b5e0f8c311f266681f5
jzj <jzj1993@gmail.com>
2020-02-02 21:48:30 +0800

    initial commit

---
+++ app/src/main/java/com/paincker/widget/constraintlayout/demo/ConstraintLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.paincker.widget.constraintlayout.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
